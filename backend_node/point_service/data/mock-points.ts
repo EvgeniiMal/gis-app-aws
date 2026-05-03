33e42a9c137597ca6bd73feaffc62a7f65c6e8de
EvgeniiMal <eugenemal88@gmail.com>
2026-05-03 11:16:52 +0200

    feat: create mock points data

---
+++ backend_node/point_service/data/mock-points.ts
@@ -0,0 +1,37 @@
+export const mockPoints = [
+  {
+    id: "1",
+    name: "Point 1",
+    description: "This is point 1",
+    latitude: 40.7128,
+    longitude: -74.006,
+  },
+  {
+    id: "2",
+    name: "Point 2",
+    description: "This is point 2",
+    latitude: 34.0522,
+    longitude: -118.2437,
+  },
+  {
+    id: "3",
+    name: "Point 3",
+    description: "This is point 3",
+    latitude: 51.5074,
+    longitude: -0.1278,
+  },
+  {
+    id: "4",
+    name: "Point 4",
+    description: "This is point 4",
+    latitude: 48.8566,
+    longitude: 2.3522,
+  },
+  {
+    id: "5",
+    name: "Point 5",
+    description: "This is point 5",
+    latitude: 35.6895,
+    longitude: 139.6917,
+  }
+];
